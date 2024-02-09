85a652d7eae844381824c732e43f601a426ae8ea
ViSombili <chisombili98@gmail.com>
2024-02-09 05:27:08 -0800

    Hello,puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+/*Puts: "Programming is like building a multilingual puzzle
+ * Return: 0
+ */
+#include <stdio.h>
+
+int main()
+{
+	puts("Programming is like building a multilingual puzzle");
+	return(0);
+}
