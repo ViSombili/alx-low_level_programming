5e25b56344072cbd33648e0ea0adc64d3ad35eba
ViSombili <chisombili98@gmail.com>
2024-02-09 05:42:00 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
-/*Puts: "Programming is like building a multilingual puzzle
- * Return: 0
- */
 #include <stdio.h>
 
-int main()
+/**
+ * main - Programming is like building a multilingual puzzle
+ *
+ * Return: returns 0
+ */
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
